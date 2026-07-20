730f44b7737f2d5fa25e08c5ca79fe302687b0c1
Alessandro Bassi <03alebassi@gmail.com>
2026-07-20 10:53:55 +0200

    Added the export of the model to the dji_captain/setup.py

--- vehicles/hardware/dji/dji_captain/setup.py
+++ vehicles/hardware/dji/dji_captain/setup.py
@@ -12,6 +12,8 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', glob('launch/*.launch*')),
+        ('share/' + package_name + '/models/M350/continuous_model', glob('models/M350/continuous_model/*')),
+        ('share/' + package_name + '/models/M350/discrete_model', glob('models/M350/discrete_model/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
